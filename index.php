91c8bde77ff425ae317e2502f8f7068f37832f3e
Simone Scognamiglio <scognac23@gmail.com>
2025-01-18 12:40:48 +0100

    eseguite modifiche

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$int = 10;  
+$float = 10.5;   
+$string = "Hello";  
+$boolean = true;   
+
+echo $int . "\n";
+echo $float . "\n";
+echo $string . "\n";
+echo $boolean . "\n";
+
+
+
+const INT_CONST = 10;        
+const FLOAT_CONST = 10.5;     
+const STRING_CONST = "Hello"; 
+const BOOLEAN_CONST = true;   
+
+echo INT_CONST . "\n";
+echo FLOAT_CONST . "\n";
+echo STRING_CONST . "\n";
+echo BOOLEAN_CONST . "\n";
+
+?>
